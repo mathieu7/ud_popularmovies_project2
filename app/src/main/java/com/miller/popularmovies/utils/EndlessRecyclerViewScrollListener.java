db540e3c210083be36b586a1e7b7dd37066fb572
Matthew Miller <mathieu7@gmail.com>
2017-03-17 18:53:06 -0400

    Add attribution to EndlessRecyclerViewScrollListener

--- app/src/main/java/com/miller/popularmovies/utils/EndlessRecyclerViewScrollListener.java
+++ app/src/main/java/com/miller/popularmovies/utils/EndlessRecyclerViewScrollListener.java
@@ -5,6 +5,9 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 
+/**
+ * Forked from ardox/codepath on Github.
+ */
 public abstract class EndlessRecyclerViewScrollListener extends RecyclerView.OnScrollListener {
     // The minimum amount of items to have below your current scroll position
     // before loading more.
